996912882e2dce78035ec8ddcf3685ce7cd8c93b
Francisco <fnaveira11@gmail.com>
2021-03-20 16:03:41 -0400

    commit tree d854d825e4058fbcdb5471c44993b6a4ee45f910

--- index.js
+++ index.js
@@ -11,6 +11,32 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = ()=>{ 
+  return tutorials.map( str => {
+  let arry = str.split(' ')
+ let string = arry.map( word =>{
+     return word[0].toUpperCase() + word.slice(1)
+    })
+
+  let result = string.join(" ")
+  return result
+  }
+)
+console.log('andArry', endArry)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//const splitArryThree = tutorials.map( arry => arry.slice(0,1).toUpperCase())
+// returns (10) ["W", "W", "I", "T", "W", "W", "I", "W", "W", "W"]
+
+//const addMaps = tutorials.map( arry => splitArryThree + arry.slice(1))
